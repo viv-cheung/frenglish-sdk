aeda1706bfc535b1ab12a3926e3759d6b7e41c22
vivian <viviann.k.c@gmail.com>
2024-10-14 16:50:51 -0400

    update workflow name

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,2 @@
-export const FRENGLISH_BACKEND_URL = 'https://api.frenglish.ai'
+//export const FRENGLISH_BACKEND_URL = 'https://api.frenglish.ai'
+export const FRENGLISH_BACKEND_URL = 'http://localhost:3001'
